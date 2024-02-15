dc17481f32008bd2b21f02614e705bea8cb53857
Jason Nordwick <jason.nordwick@gmail.com>
2024-02-14 19:04:18 -0500

    added ex

--- src/main.zig
+++ src/main.zig
@@ -4,26 +4,32 @@ const std = @import("std");
 // https://github.com/grantjenks/python-runstats
 
 /// Makes a running cummulaive stats struct.
-/// ST: The type of the stat variables. Unless you have very speial
+/// Sample: The type of the stat variables. Unless you have very speial
 /// needs, this should be a float. The significand size should not
 /// be larger than your sample size.
-pub fn RunningStats(comptime ST: type) type {
+pub fn RunningStats(comptime SampleT: type) type {
     return struct {
         const This = @This();
+        pub const Sample = SampleT;
 
+        // is mixed with calculations against negatve numbers so just easier tha i32 or casting a lo9t
+        /// number of samples
         n: i64,
-        min: ST,
-        max: ST,
-        eta: ST,
-        tau: ST,
-        rho: ST,
-        phi: ST,
+        /// lowest sample. uses @min for comparisons
+        min: Sample,
+        /// highest sample. uses @max for comparisons
+        max: Sample,
+        // these are used to keep running values and from these the moments can be calculatesd
+        eta: Sample,
+        tau: Sample,
+        rho: Sample,
+        phi: Sample,
 
         pub fn new() This {
             return .{
                 .n = 0,
-                .min = std.math.inf(ST),
-                .max = -std.math.inf(ST),
+                .min = std.math.inf(Sample),
+                .max = -std.math.inf(Sample),
                 .eta = 0.0,
                 .phi = 0.0,
                 .tau = 0.0,
@@ -31,7 +37,7 @@ pub fn RunningStats(comptime ST: type) type {
             };
         }
 
-        pub fn update(s: *This, x: ST) void {
+        pub fn update(s: *This, x: Sample) void {
             s.n += 1;
             s.min = @min(s.min, x);
             s.max = @max(s.max, x);
@@ -51,46 +57,48 @@ pub fn RunningStats(comptime ST: type) type {
             s.rho += term;
         }
 
-        fn n_float(s: *const This) ST {
+        fn n_float(s: *const This) Sample {
             return @floatFromInt(s.n);
         }
 
-        fn n_adj_float(s: *const This, d: i64) ST {
+        fn n_adj_float(s: *const This, d: i64) Sample {
             return @floatFromInt(s.n + d);
         }
 
-        pub fn min(s: *const This) ST {
+        pub fn min(s: *const This) Sample {
             return s.min;
         }
 
-        pub fn max(s: *const This) ST {
+        pub fn max(s: *const This) Sample {
             return s.max;
         }
 
-        pub fn mean(s: *const This) ST {
+        pub fn mean(s: *const This) Sample {
             return s.eta;
         }
 
-        pub fn variance(s: *const This) ST {
+        pub fn variance(s: *const This) Sample {
             return s.variance_mdof(1);
         }
 
-        pub fn variance_mdof(s: *const This, dof: u32) ST {
-            return s.rho / s.n_float(-@as(i64, dof));
+        pub fn variance_mdof(s: *const This, dof: u32) Sample {
+            const fdof: f64 = dof;
+            return s.rho / (s.n_float() - fdof);
         }
 
-        pub fn stdev(s: *const This) ST {
+        pub fn stdev(s: *const This) Sample {
             return s.stdev_mdof(1);
         }
-        pub fn stdev_mdof(s: *const This, dof: u32) ST {
+
+        pub fn stdev_mdof(s: *const This, dof: u32) Sample {
             return @sqrt(s.variance_mdof(dof));
         }
 
-        pub fn skew(s: *const This) ST {
+        pub fn skew(s: *const This) Sample {
             return @sqrt(s.n_float()) * s.tau / (s.rho * @sqrt(s.rho));
         }
 
-        pub fn kurtosis(s: *const This) ST {
+        pub fn kurtosis(s: *const This) Sample {
             return s.n_float() * s.phi / (s.rho * s.rho) - 3.0;
         }
 
@@ -138,7 +146,7 @@ pub fn RunningStats(comptime ST: type) type {
             s.phi = tot_phi;
         }
 
-        pub fn smul(s: *This, scalar: ST) void {
+        pub fn smul(s: *This, scalar: Sample) void {
             s.n *= scalar;
             s.phi *= scalar;
             s.tau *= scalar;
@@ -148,8 +156,97 @@ pub fn RunningStats(comptime ST: type) type {
     };
 }
 
+pub fn ExpStats(SampleT: type) type {
+    return struct {
+        const This = @This();
+        pub const Sample = SampleT;
 
-// --- --- TESTS --- ---
+        /// must be 0 < decay < 1
+        decay: Sample = 0.9,
+        mean: Sample,
+        variance: Sample,
+
+        pub fn init(decay: f64) This {
+            return .{.decay = decay, .mean = 0, .variance = 0};
+        }
+
+        pub fn update(s: *This, x: Sample) void {
+            const alpha = 1.0 - s.decay;
+            const diff = x - s.mean;
+            const incr = alpha * diff;
+            s.variance += alpha * (s.decay * diff * diff - s.variance);
+            s.mean += incr;
+        }
+
+        pub fn mean(s: *const This) Sample {
+            return s.mean;
+
+        }
+
+        pub fn variance(s: *const This) Sample {
+            return s.variance;
+        }
+
+        pub fn stdev(s: *const This) Sample {
+            return @sqrt(s.variance);
+        }
+
+        pub fn add(s: *This, o: *const This) void {
+            s.mean += o.mean;
+            s.variance += o.variance;
+        }
+    };
+}
+
+pub fn Corr(SampleT: type) type {
+    return struct {
+        const This = @This();
+        const Sample = SampleT;
+
+        xstats:RunningStats(Sample),
+        ystats:RunningStats(Sample),
+        xy: Sample,
+        n: i64,
+
+        pub fn update(s: *This, x: Sample, y: Sample) void {
+            s.xy += (s.xstats.mean() - x)
+                * (s.ystats.mean() - y)
+                * s.n / (s.n + 1);
+            s.xstats.update(x);
+            s.ystats.update(y);
+            s.n += 1;
+        }
+
+        pub fn slope(s: *const This) Sample {
+            return slope_dof(s, 1.0);
+        }
+
+        pub fn slope_dof(s: *const This, dof: Sample) Sample {
+            const xx = s.xstats.variance_mdof(dof) * (s.count - dof);
+            return s.xy / xx;
+        }
+
+        pub fn intercept(s: *const This) Sample {
+            return intercept(s, 1.0);
+        }
+
+        pub fn intercept_dof(s: *const This, dof: Sample) Sample {
+            return s.ystats.mean() - s.slope(dof) * s.xstats.mean();
+        }
+
+        pub fn corr(s: *const This, dof: Sample) Sample {
+            return corr_dof(s, 1.0);
+        }
+
+        pub fn corr_dof(s: *const This, dof: Sample) Sample {
+            const term = s.xstats.stdev_mdof(dof) * s.ystats.stdev(dof);
+            return s.xy / ((s.n - dof) * term);
+        }
+    };
+}
+
+
+// --- --- tests --- ---
 // yeah some day this will happen
 
 test "make it" {
